988f5fcea0446cdb9d93e64120b7292ce4c71fc6
shivam-dubey-personal <shivamdubeyagra@gmail.com>
2025-10-27 23:16:30 +0530

    initial commit: setup turborepos project prisme client exp

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,7 @@
 import Image, { type ImageProps } from "next/image";
 import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
+import { client } from "@repo/db/client";
 
 type Props = Omit<ImageProps, "src"> & {
   srcLight: string;
@@ -9,7 +10,6 @@ type Props = Omit<ImageProps, "src"> & {
 
 const ThemeImage = (props: Props) => {
   const { srcLight, srcDark, ...rest } = props;
-
   return (
     <>
       <Image {...rest} src={srcLight} className="imgLight" />
